4673c5e15aee169829025b6c2d630cb45f64443f
Darkninjaspace <andersonhan8@gmail.com>
2024-10-16 15:05:41 -0700

    thng 1

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ train = ConditionalCategorical(
 
 worldCup = Categorical(
     [
-        [0.7,0.3] #chance if world cup affected coffee shop being open or not
+        [0.1,0.9] #chance if world cup affected coffee shop being open or not
     ]
 )
 
@@ -48,15 +48,62 @@ coffeeShop = ConditionalCategorical(
     [
         [
             [
-                [0.7,0.3], #world cup affected train late shop closed, world cup affected train late shop open
-                [0.5,0.5], #world cup affected train on time shop closed, world cup affected train on time shop open
+                [0.2,0.8], #world cup affected train late shop closed, world cup affected train late shop open
+                [0.3,0.7], #world cup affected train on time shop closed, world cup affected train on time shop open
             ],
             [
-                [0.4,0.6], #world cup not affected train on time shop closed, world cup affected train on time shop open
-                [0.3,0.7], #world cup not affected train on time shop closed, world cup affected train on time shop open
+                [0.7,0.3], #world cup not affected train on time shop closed, world cup affected train on time shop open
+                [0.8,0.2], #world cup not affected train on time shop closed, world cup affected train on time shop open
             ],
         ]
     ]
+)
+
+reception = ConditionalCategorical(
+    [
+        [
+            [
+                [0.95,0.05], #no rain yes interference, no rain no interference
+            ],
+            [
+                [0.9,0.1], #light rain yes interference, light rain no interference
+            ],
+            [
+                [0.75,0.25], #heavy rain yes interference, heavy rain no interference
+            ],
+        ]
+    ]
+)
+
+carCrash = ConditionalCategorical(
+    [
+        [
+            [
+                [0.2,0.8], #no rain world cup affected crash, no rain world cup affected no crash
+                [0.1,0.9], #no rain world cup not affected crash, no rain world cup not affected no crash
+            ],
+            [
+                [0.4,0.6], #light rain world cup affected crash, light rain world cup not affected no crash
+                [0.3,0.7], #light rain world cup not affected crash, light rain world cup not affected no crash
+            ],
+            [
+                [0.65,0.35], #heavy rain world cup affected crash, heavy rain world cup not affected no crash
+                [0.5,0.5], #heavy rain world cup not affected crash, heavy rain world cup not affected no crash
+            ],
+        ]
+    ]
+)
+
+survival = Categorical(
+    [
+        [0.01,0.99], #chance of car crash death at low speeds, chance of car crash survival at low speeds
+    ],
+)
+
+womanDoctor = Categorical(
+    [
+        [0.49,0.51], #chance of female doctor, chance of male doctor
+    ],
 )
 
 model = BayesianNetwork()
@@ -71,3 +118,6 @@ model.add_edge (worldCup, coffeeShop)
 
 #world cup, train affect coffee shop closed
 #rain, maintenence affect train
+#rain affects cell reception 
+#world cup and rain affect car crash
+#chance that doctor in london that is single is woman
--- probabilites.py
+++ probabilites.py
@@ -7,6 +7,11 @@ maintenence_values = ["maintenence","no maintenence"]
 train_values = ["on time","late"]
 worldCup_values = ["affected","not affected"]
 coffeeShop_values = ["open","closed"]
+reception_values = ["interference","no interference"]
+carCrash_values = ["crash","no crash"]
+survival_values = ["survival","death"]
+womanDoctor_values = ["male","female"]
+
 probability = model.probability(
     torch.as_tensor(
         [
@@ -15,7 +20,11 @@ probability = model.probability(
                 maintenence_values.index("maintenence"),
                 train_values.index("late"),
                 worldCup_values.index ("affected"),
-                coffeeShop_values.index ("open")
+                coffeeShop_values.index ("closed"),
+                reception_values.index ("interference"),
+                carCrash_values ("crash"),
+                survival_values ("survival"),
+                womanDoctor_values ("female")
             ],
         ]
     )
