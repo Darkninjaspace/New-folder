f7fe40147eae2034c1b62a97751a175486c50e41
Darkninjaspace <andersonhan8@gmail.com>
2024-10-24 11:48:15 -0700

    I dont know how this works but assignment 7 is done???

--- advCompSci7.py
+++ advCompSci7.py
@@ -3,6 +3,16 @@ from pomegranate.markov_chain import*
 from pomegranate.hmm import DenseHMM
 import numpy
 
+probs_Sophia = torch.tensor([
+    [0.7,0.3]
+    ])
+sophia = Categorical(probs = probs_Sophia)
+
+probs_Kitchen = torch.tensor([
+    [0.2,0.8]
+    ])
+kitchen = Categorical(probs = probs_Kitchen)
+
 probs_Start = torch.tensor([
     [0.5,0.5]
     ])
@@ -14,20 +24,11 @@ probs_Transitions = torch.tensor([
     ])
 transistions = ConditionalCategorical(probs= [probs_Transitions])
 
-model = MarkovChain([start,transistions])
-model.sample(1)
+states = [sophia,kitchen]
+edges = [[0.7,0.3],[0.2,0.8]]
+starts = [0.5,0.5]
 
-print(model.sample)
+model = DenseHMM(states, edges=edges, starts=starts)
+observations = numpy.array([[[0],[0],[1],[0],[0],[0],[0],[1],[1]]])
 
-"my mom"
-"traffic"
-
-[0.7,0.3]#Sophia start: Sophia, Kitchen 
-[0.2,0.8]#Kitchen start: Sophia, Kitchen
-
-probs_Sophia = torch.tensor([
-    [0.7,0.3]
-    ])
-probs_Kitchen = torch.tensor([
-    [0.2,0.8]
-    ])
+print (model.predict(observations))
